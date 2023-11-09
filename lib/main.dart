d3eb654c90636d21a6bbf89d09239dd0d8904aab
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-11-09 10:46:26 +0530

    main implemented

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:codexia_learning/course/screen/course_show_screen.dart';
+import 'package:codexia_learning/course/service/course_service.dart';
+import 'package:codexia_learning/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  const fatalError = true;
+
+  FlutterError.onError = (errorDetails) {
+    if (fatalError) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+      // ignore: dead_code
+    } else {
+      FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    }
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    if (fatalError) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      // ignore: dead_code
+    } else {
+      FirebaseCrashlytics.instance.recordError(error, stack);
+    }
+    return true;
+  };
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ShowCourseScreen(
+        courseService: CourseService(),
+      ),
+    );
+  }
+}
