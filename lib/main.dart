f14e8daf9f5804f38cf07ebde72fd9e1e1447259
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-11-09 11:01:37 +0530

    todo warning

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      //TODO for testing only. Replace with AuthScreen later
       home: ShowCourseScreen(
         courseService: CourseService(),
       ),
