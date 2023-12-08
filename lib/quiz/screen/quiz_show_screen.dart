71991690e30f1adf96b42e1eb806bad6ef8c0ffa
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-12-08 11:31:31 +0530

    RadioButtonWidget call

--- lib/quiz/screen/quiz_show_screen.dart
+++ lib/quiz/screen/quiz_show_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:codexia_learning/quiz/model/quiz_model.dart';
 import 'package:codexia_learning/quiz/provider/quiz_provider.dart';
+import 'package:codexia_learning/quiz/widget/quiz_timer_widget.dart';
+import 'package:codexia_learning/quiz/widget/radio_list_tile_widget.dart';
 import 'package:codexia_learning/shared/colour_const.dart';
 import 'package:codexia_learning/shared/string_const.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -17,13 +21,18 @@ class QuizShowScreen extends StatefulWidget {
 
 class _QuizShowScreenState extends State<QuizShowScreen> {
   late QuizProvider quizProvider;
-  List<String> selectedOption = [];
+  String selectOption = '';
+  bool isOptionSelected = false;
+  bool isAnswerCorrect = false;
+  var question = 0;
 
   @override
   void initState() {
     super.initState();
     quizProvider = Provider.of<QuizProvider>(context, listen: false);
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -45,18 +54,18 @@ class _QuizShowScreenState extends State<QuizShowScreen> {
                 List<Quiz> quizList = [];
                 DataSnapshot dataSnapshot = snapshot.data!.snapshot;
                 if (dataSnapshot.exists) {
-                  final map = dataSnapshot.value as Map<String, dynamic>;
+                  final map = dataSnapshot.value as Map<dynamic, dynamic>;
 
-                  map.forEach((key, value) {
-                    var quiz = Quiz(
-                      chapterId: value['chapterId'] ?? '',
-                      option1: value['option1'] ?? '',
-                      option2: value['option2'] ?? '',
-                      option3: value['option3'] ?? '',
-                      option4: value['option4'] ?? '',
-                      correctOption: value['correctOption'] ?? '',
-                      question: value['question'] ?? '',
-                      questionId: value['questionId'] ?? '',
+                    map.forEach((key, value) {
+                      var quiz = Quiz(
+                        chapterId: value['chapterId'] ?? '',
+                        option1: value['option1'] ?? '',
+                        option2: value['option2'] ?? '',
+                        option3: value['option3'] ?? '',
+                        option4: value['option4'] ?? '',
+                        correctOption: value['correctOption'] ?? '',
+                        question: value['question'] ?? '',
+                        questionId: value['questionId'] ?? '',
                     );
                     quizList.add(quiz);
                   });
@@ -67,71 +76,20 @@ class _QuizShowScreenState extends State<QuizShowScreen> {
                   itemBuilder: (context, index) {
                     return Padding(
                       padding:
-                          const EdgeInsets.only(left: 40, right: 40, top: 10),
+                      const EdgeInsets.only(left: 40, right: 40, top: 10),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Que.  ${quizList[index].question}',
-                            style: const TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          RadioListTile<String>(
-                            title: Text(
-                              quizList[index].option1,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const QuizTimerWidget(),
+                            const SizedBox(
+                              height: 20,
                             ),
-                            value: 'A',
-                            groupValue: quizList[index].correctOption,
-                            onChanged: (value) {},
-                          ),
-                          RadioListTile<String>(
-                            title: Text(
-                              quizList[index].option2,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
+                            RadioListTileWidget(
+                              chapterId: widget.chapterId,
+                              quizList: quizList,
+                              index: index,
                             ),
-                            value: 'B',
-                            groupValue: quizList[index].correctOption,
-                            onChanged: (value) {},
-                          ),
-                          RadioListTile<String>(
-                            title: Text(
-                              quizList[index].option3,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                            value: 'C',
-                            groupValue: quizList[index].correctOption,
-                            onChanged: (value) {},
-                          ),
-                          RadioListTile<String>(
-                            title: Text(
-                              quizList[index].option4,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                            value: 'D',
-                            groupValue: quizList[index].correctOption,
-                            onChanged: (value) {},
-                          ),
-                          Text(
-                            'Correct Answer: ${quizList[index].correctOption}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
+                          ],
                       ),
                     );
                   },
